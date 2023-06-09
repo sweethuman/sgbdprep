c073aba3e8278819bdcb31a0da51547ad3a99fe1
sweethuman <gheo@sweethuman.tech>
2023-06-09 16:16:58 +0300

    more stuff

--- infra/create.sql
+++ infra/create.sql
@@ -1,2 +1,8 @@
 ﻿CREATE DATABASE sgbd;
 USE sgbd;
+CREATE SCHEMA test;
+CREATE TABLE test.test (
+                      name VARCHAR(50) NOT NULL,
+                      salary DECIMAL(18,2) NOT NULL,
+                      location VARCHAR(50) NOT NULL
+);
